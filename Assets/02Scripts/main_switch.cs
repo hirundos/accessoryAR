9120b704b36d9444999bb23c0178c3663885894e
hirundos <dkdus591@gmail.com>
2020-10-30 10:55:42 +0900

    no message

---
+++ Assets/02Scripts/main_switch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+public class main_switch : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+		if (Input.GetMouseButtonDown(0))
+        {
+            Application.LoadLevel("second");
+        }
+		
+
+        /*클릭시 main 신을 불러오라는 의미 */
+    }
+}
